480082b93e029e184c9bbdbb3660c3390adb6af6
kiwi107 <119266066+kiwi107@users.noreply.github.com>
2024-11-01 18:21:55 +0200

    added summmation function

---
+++ 21P0223_karimsherif_main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int summation(int a, int b) {
+    return a + b;
+}
+
+int main() {
+    int x = 12, y = 123;
+    cout << "Sum: " << summation(x, y) << endl;
+    return 0;
+}
